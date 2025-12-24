dac0065b94f7998616a75df4f1fc13ca40e1b34f
shofwa <shofwatunisa07@gmail.com>
2025-12-25 01:40:34 +0700

    Update automate_Shofwatun_Nisa.py

--- preprocessing/automate_Shofwatun_Nisa.py
+++ preprocessing/automate_Shofwatun_Nisa.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import string
 import nltk
+import os
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.preprocessing import LabelEncoder
@@ -39,8 +40,6 @@ def preprocess():
 
     df_final = df[['processed_text', 'label_encoded']]
 
-    # Pastikan folder ada sebelum save
-    import os
     os.makedirs(os.path.dirname(OUTPUT_PATH), exist_ok=True)
     df_final.to_csv(OUTPUT_PATH, index=False)
 
